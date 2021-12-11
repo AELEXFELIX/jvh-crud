f4c1f426df436b092ab9e83ca074c9b99cf6d6a7
AELEXFELIX <89672692+AELEXFELIX@users.noreply.github.com>
2021-12-11 12:38:15 -0400

    Create update.php

---
+++ update.php
@@ -0,0 +1,19 @@
+# jvh-crud
+trabajo
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$cod_estudiante=$_POST['cod_estudiante'];
+$dni=$_POST['dni'];
+$nombres=$_POST['nombres'];
+$apellidos=$_POST['apellidos'];
+
+$sql="UPDATE alumno SET  dni='$dni',nombres='$nombres',apellidos='$apellidos' WHERE cod_estudiante='$cod_estudiante'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: alumno.php");
+    }
+?>
